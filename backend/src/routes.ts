ab605a2c7c6c93ef78a248b6a376d3991dccf7a7
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2021-10-18 21:59:28 -0300

    Commit save first class

---
+++ backend/src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+
+const router = Router()
+const authenticateUserController = new AuthenticateUserController()
+
+router.post('/authenticate', authenticateUserController.handle)
+
+export { router }
